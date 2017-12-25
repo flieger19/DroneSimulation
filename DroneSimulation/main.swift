4c526d9735c95357ae2dcabf810cfe9eff475486
Constantin Dullo <constantin.dullo@gmail.com>
2017-12-25 19:36:58 +0100

    Initial Commit

---
+++ DroneSimulation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DroneSimulation
+//
+//  Created by Constantin Dullo on 25.12.17.
+//  Copyright © 2017 Constantin Dullo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
